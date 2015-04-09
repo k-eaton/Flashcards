83ad098ce293fc833dd408d1e4a1209a8ac4d039
Katrina Eaton <katrina.eaton@gmail.com>
2015-04-09 11:55:52 -0700

    layout controller scaffold

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -2,10 +2,30 @@ get '/decks' do
   erb :'decks/decks'
 end
 
-get '/deck/:id/cards' do
-  erb :cards
+get '/decks/:id' do
+  erb :'decks/deck'
 end
 
-get '/' do
+get '/decks/new' do
+  erb :'decks/new'
+end
 
+post '/decks/new' do
+  redirect '/decks'
+end
+
+get '/decks/:id/edit' do
+  erb :'decks/edit'
+end
+
+put '/decks/:id' do
+  redirect '/decks/:id'
+end
+
+get '/decks/:id/delete' do
+  erb :'decks/delete'
+end
+
+delete '/decks/:id' do
+  redirect '/decks'
 end
