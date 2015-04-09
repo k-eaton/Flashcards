dced9843c228e8506b0298f4c4a7b5cef148afd3
Katrina Eaton <katrina.eaton@gmail.com>
2015-04-09 16:23:12 -0700

    edit routes

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,17 +1,21 @@
 get '/decks' do
+  @decks = Deck.all
   erb :'decks/decks'
 end
 
 get '/decks/:id' do
+  @deck = Deck.where(id: params[:id]).first
+  @cards = Card.where(deck_id: params[:id])
   erb :'decks/deck'
 end
 
 get '/decks/new' do
+
   erb :'decks/new'
 end
 
-post '/decks/new' do
-  redirect '/decks'
+post '/decks' do
+  redirect '/decks/cards/new'
 end
 
 get '/decks/:id/edit' do
