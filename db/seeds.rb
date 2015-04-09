4144bac73d43753cc468916f0edd388fb77fb27f
Katrina Eaton <katrina.eaton@gmail.com>
2015-04-09 12:30:15 -0700

    update seed file

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+require 'faker'
+
+5.times do
+  Deck.create(name: Faker::Lorem.words.join(" "))
+end
+
+10.times do
+  Card.create(deck_id: rand(1..5), cardname: Faker::Lorem.word, question: Faker::Lorem.words(5).join(" "), correct_answer: "correct answer", incorrect_answer1: "incorrect 1", incorrect_answer2: "incorrect 2", incorrect_answer3: "incorrect 3")
+end
