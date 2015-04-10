b8694e9dac6df90e5ef6655b5e8ac6c5acd195ad
EclecticKlos <dsklos@gmail.com>
2015-04-10 16:11:37 -0700

    Gameplay works with score

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -35,31 +35,45 @@ end
 
 get '/play/decks/show' do
   @all_decks = Deck.all
+  session[:score] = 0
 
   erb :"decks/play_show_decks"
 end
 
-get '/play/decks/:deck_id/cards/:card_id' do
-  @message = "Welcome! Pick the right answer"
+get '/play/decks/:deck_id/cards/:card_id/:que' do
   @deck = Deck.find(params[:deck_id])
   @all_cards = @deck.cards.all
   @card_iterator = params[:card_id].to_i
-  if true
-    if true # == @this_card.correct_answer
+  if params[:card_id].to_i < (@all_cards.length - 1)
+    if params[:que] == "first"
+      @message = "Welcome! Pick the right answer"
+    elsif params[:que] == "correct" #@this_card.correct_answer
       @message = "Correct! How about this one?"
       @card_iterator += 1
-    else
+    elsif params[:que] == "incorrect"
       @message = "WRONG! Maybe you'll have better luck with this one:"
       @card_iterator += 1
+    else
+      @message = "Error. Don't ya hate that? Start over again."
     end
-  end
-
   erb :"decks/play_card"
+  else
+    @message = "Nice job!"
+    @completed_message = "You got #{session[:score]} out of #{@all_cards.length}"
+  erb :"decks/completed"
+  end
 end
 
 post '/play/decks/:deck_id/cards/:card_id' do
-  p "&"*80
-  p params
-  redirect "/play/decks/#{params[:deck_id]}/cards/#{params[:card_id]}"
+    if params[:selected] == "correct_answer"
+      session[:score] += 1
+      p "&"*80
+      p session[:score]
+      redirect "/play/decks/#{params[:deck_id]}/cards/#{params[:card_id]}/correct"
+    elsif
+      params[:selected] != "correct_answer"
+      p session[:score]
+      redirect "/play/decks/#{params[:deck_id]}/cards/#{params[:card_id]}/incorrect"
+    end
 end
 
